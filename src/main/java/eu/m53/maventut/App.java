57e3f3ee409eed7983801a3d637d6c51692867f8
Mike Pountney <Mike.Pountney@gmail.com>
2010-02-23 01:59:03 +0000

    adding first maven project

---
+++ src/main/java/eu/m53/maventut/App.java
@@ -0,0 +1,13 @@
+package eu.m53.maventut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
